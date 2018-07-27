182cee8a2a1d2fd14d03002e0f33c0cf593291fc
chenyu15 <chenyu15@58ganji.com>
2018-07-27 17:13:56 +0800

    headToast

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 + 安全软键盘 keyboard
 + 上拉菜单 upMenu
 + 自定义tab 可滑动 tabs
-
++ 头部下拉toast
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,72 +3,77 @@
 const app = getApp()
 
 Page({
-    data: {
-        motto: 'Hello World',
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
 
-        toastData:{
-            isShow:false
-        }
-
-    },
-    //事件处理函数
-    bindViewTap: function () {
-        wx.navigateTo({
-            url: '../logs/logs'
-        })
-    },
-    onLoad: function () {
-
-    },
-    getUserInfo: function (e) {
-
-    },
-    timePicker: function () {
-        wx.navigateTo({
-            url: '../com/timePicker/timePicker'
-        })
-    },
-    leftSwiper: function () {
-        wx.navigateTo({
-            url: '../com/leftSwiper/leftSwiper'
-        })
-    },
-    toast:function () {
-        let self=this;
-        console.log(1)
-        app.showToast('wosho',self,2000)
-    },
-    observerToast:function () {
-        let self=this;
-        self.setData({
-            toastData:{
-                isShow:true,
-                delay:2000,
-                msg:'我是自定义组件'
-            }
-        })
-    },
-    goModal:function () {
-        wx.navigateTo({
-          url: '../com/modal/modal'
-        })
-    },
-    goKeyoard:function () {
-        wx.navigateTo({
-            url: '../com/keyboard/keyboard'
-        })
-    },
-    goupMenu:function () {
-        wx.navigateTo({
-            url: '../com/upMenu/upMenu'
-        })
-    },
-    goTabs:function () {
-        wx.navigateTo({
-            url: '../com/tabs/tabs'
-        })
+    toastData: {
+      isShow: false
     }
+
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+
+  },
+  getUserInfo: function (e) {
+
+  },
+  timePicker: function () {
+    wx.navigateTo({
+      url: '../com/timePicker/timePicker'
+    })
+  },
+  leftSwiper: function () {
+    wx.navigateTo({
+      url: '../com/leftSwiper/leftSwiper'
+    })
+  },
+  toast: function () {
+    let self = this;
+    console.log(1)
+    app.showToast('wosho', self, 2000)
+  },
+  observerToast: function () {
+    let self = this;
+    self.setData({
+      toastData: {
+        isShow: true,
+        delay: 2000,
+        msg: '我是自定义组件'
+      }
+    })
+  },
+  goModal: function () {
+    wx.navigateTo({
+      url: '../com/modal/modal'
+    })
+  },
+  goKeyoard: function () {
+    wx.navigateTo({
+      url: '../com/keyboard/keyboard'
+    })
+  },
+  goupMenu: function () {
+    wx.navigateTo({
+      url: '../com/upMenu/upMenu'
+    })
+  },
+  goTabs: function () {
+    wx.navigateTo({
+      url: '../com/tabs/tabs'
+    })
+  },
+  goheadToast:function () {
+    wx.navigateTo({
+      url: '../com/headToast/headToast'
+    })
+  }
 })
