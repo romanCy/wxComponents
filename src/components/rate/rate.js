54cfeecf47ce9ca00a9c2269a319407901658630
chenyu15 <chenyu15@58ganji.com>
2018-07-30 15:08:35 +0800

    rate

--- src/components/rate/rate.js
+++ src/components/rate/rate.js
@@ -12,12 +12,42 @@ Component({
    */
   data: {
 
+    count:5,
+    selectCount:0
+
   },
+  _left:0,
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    tm:function (e) {
+      console.log(e.touches[0].clientX)
+      let clX=e.touches[0].clientX-this._left
+      console.log(clX);
+      console.log(clX/60)
+      let self=this;
+      self.setData({
+        selectCount:clX/60
+      })
+    },
+    tp:function (e) {
+      let clX=e.touches[0].clientX-this._left
+      console.log(typeof this._left);
+      let self=this;
+      self.setData({
+        selectCount:clX/60
+      })
+    }
+  },
+  ready:function () {
+    let self=this
+    var query = wx.createSelectorQuery().in(this)
+    query.select('#the-id').boundingClientRect(function(res){
+      console.log(res)
+      self._left=res.left
+      console.log(this._left)
+    }).exec()
   }
 })
